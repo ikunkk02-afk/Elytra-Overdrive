477cb03988ee3b3eb758046495b4a8c42e5d8318
ikunkk02-afk <ikunkk02@gmail.com>
2026-07-11 16:27:40 +0800

    feat: add overdrive enchantment

---
+++ src/main/java/io/github/ikunkk02/elytraoverdrive/enchantment/OverdriveEnchantments.java
@@ -0,0 +1,40 @@
+package io.github.ikunkk02.elytraoverdrive.enchantment;
+
+import io.github.ikunkk02.elytraoverdrive.ElytraOverdrive;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+
+public final class OverdriveEnchantments {
+	public static final ResourceKey<Enchantment> OVERDRIVE = ResourceKey.create(
+			Registries.ENCHANTMENT,
+			ElytraOverdrive.id("overdrive")
+	);
+
+	private OverdriveEnchantments() {
+	}
+
+	public static boolean hasOverdrive(LivingEntity entity, ItemStack chestStack) {
+		if (!chestStack.is(Items.ELYTRA)) {
+			return false;
+		}
+
+		var enchantment = entity.registryAccess()
+				.registryOrThrow(Registries.ENCHANTMENT)
+				.getHolderOrThrow(OVERDRIVE);
+		return EnchantmentHelper.getItemEnchantmentLevel(enchantment, chestStack) > 0;
+	}
+
+	public static boolean canUseOverdriveElytra(LivingEntity entity) {
+		ItemStack chestStack = entity.getItemBySlot(EquipmentSlot.CHEST);
+		return chestStack.is(Items.ELYTRA)
+				&& ElytraItem.isFlyEnabled(chestStack)
+				&& hasOverdrive(entity, chestStack);
+	}
+}
