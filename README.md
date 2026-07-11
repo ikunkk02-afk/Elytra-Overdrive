8086df2a46e91cdc408244754c0effab13bcecc5
ikunkk02-afk <ikunkk02@gmail.com>
2026-07-11 16:19:54 +0800

    chore: initialize Fabric 1.21.1 project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Elytra Overdrive
+
+## Setup
+
+For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
+
+## License
+
+This project is available under the MIT License.
---
+++ src/client/java/io/github/ikunkk02/elytraoverdrive/client/ElytraOverdriveClient.java
@@ -0,0 +1,10 @@
+package io.github.ikunkk02.elytraoverdrive.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElytraOverdriveClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
